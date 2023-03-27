85e4abbafe00701b7ae9a3b76c9eb1bbaa5d08d8
manuaba42 <ssedanayoga3@gmail.com>
2023-03-27 22:45:29 +0800

    decode commit

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,4 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -12,12 +11,21 @@ def encrypt(alfa, move):
     newalfa = alphabet[newindex]
     return newalfa
 
+def decrypt(alfa, move):
+    index = alphabet.index(alfa)
+    newindex = index - move
+    newalfa = alphabet[newindex]
+    return newalfa
+
 if direction == 'encode':
-    # array = text.split()
     newarr = []
-    # print(array)
     for arr in text:
         i = encrypt(arr, shift)
-        newarr.append(i)
-    
+        newarr.append(i)   
+    print("".join(newarr))
+else:
+    newarr = []
+    for arr in text:
+        i = decrypt(arr, shift)
+        newarr.append(i)   
     print("".join(newarr))
